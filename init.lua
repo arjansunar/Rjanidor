8237ef96ab4d91d22ecbf09b8c49e2c0faad089f
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-08-15 20:13:19 +0545

    init: PDE setup with lazy

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- [[ default settings ]]
+require 'config.settings'
+
+-- [[ Package manager]]
+require 'config.lazy'
+
+-- [[ Setup lazy packages]]
+require 'config.setup'
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
